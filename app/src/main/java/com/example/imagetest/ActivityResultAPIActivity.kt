fb9ee0d14522ea3c1b347ffbf675943e2d72d7a5
neoneoneo123 <gouxrunner@gmail.com>
2024-07-10 08:35:13 +0900

    feat: ActivityResultAPI 방식 작성

--- app/src/main/java/com/example/imagetest/ActivityResultAPIActivity.kt
+++ app/src/main/java/com/example/imagetest/ActivityResultAPIActivity.kt
@@ -2,19 +2,22 @@ package com.example.imagetest
 
 import android.net.Uri
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.example.imagetest.databinding.ActivityResultApiactivityBinding
 
 class ActivityResultAPIActivity : AppCompatActivity() {
+
+    private val binding: ActivityResultApiactivityBinding by lazy {
+        ActivityResultApiactivityBinding.inflate(layoutInflater)
+    }
+
     // Activity Result 등록
     private val getContent = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
         uri?.let {
             // Uri 이미지 view에 표시
-            val imageView: ImageView = findViewById(R.id.imageView)
-            imageView.setImageURI(uri)
+            binding.imageView.setImageURI(uri)
         } ?: run {
             Toast.makeText(this, "No image selected", Toast.LENGTH_SHORT).show()
         }
@@ -22,10 +25,9 @@ class ActivityResultAPIActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
-        val button: Button = findViewById(R.id.button)
-        button.setOnClickListener {
+        binding.button.setOnClickListener {
             // 갤러리 열기
             getContent.launch("image/*")
         }
